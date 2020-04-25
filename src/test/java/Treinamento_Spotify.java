c9a9e1e55bed11e947a3007b864d054d7c20acda
unknown <erika.machado@prestador.com.br>
2020-04-25 17:45:45 -0300

    Adcionado teste de consultar música e artista

--- src/test/java/Treinamento_Spotify.java
+++ src/test/java/Treinamento_Spotify.java
@@ -1,17 +1,82 @@
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
 
 public class Treinamento_Spotify {
 
-    public static String token = "BQDVaR7GzMYae43yBoa0_4y8C8rPeLdKspMt6QxMg_I_Ghc2QOpmEY5d7uPLtJTlGbyx7zed_GR9uA2_oZ0RzPDbx-pIotOtyYmK5AT-IjjrIdWJzb4vpbz5gyItJtgy22Qj3K3HjyxIdyHTHeZGltyHGxvoLD6LxfKQVqFZMNY9YP2U6ZqmNLj1hZBiMGsM1X_vlo6Gezrnbj1XbHGVheTRp4G2D_i5LGnfRd18miLChIbaT-vEvmMVD3ZpkjeKQctkieoCrVJ5z0-hLCei-SHB-quE0QDwBmOfmQ";
+    public static String token = "BQBnPpXV9f5reFoDevXI2KWP8GkpttcrbBMV5Baw1pZkVljSvidgKXgJFU8PPDWJw6AeDD8xRUUxIMWylQoMX2SYzjAH7mVguMlKwOy8LBsolCfSsiuTigJcDw7GMVh4jUSS0qz4fvBp6hfEUEN-IHCRfEeE87XSzaF-5366gBCz02tqoe4KpX2nXc_zjrXPcW-lmyIsEDgiLQM2rmXkOXdhvIPUXuf8EpAQuKcP_876OgpBw8uQ3nYODNTrv-HdTEJCRnbeMvvwdLnvaJVxG2GTcXuua5ZiXThWoA";
     public static String URL = "https://api.spotify.com/v1";
     public static Response response;
+    public static String nomePlayList = "Teste Postman";
+    public static ArrayList list;
+    public static  String idPlayList;
+    public static  String idMusica;
+    public static  String nomeArtista = "Omnitica";
+    String nomeMusica = "Dubwoofer Substep";
+    int index;
 
     @Test
     public void buscarPlayList() {
+        busPlayListRequest();
+        if (response.statusCode() != 401) {
+            Assert.assertEquals(response.statusCode(), 200);
+            Assert.assertTrue(verificaPorNome(nomePlayList));
+        } else {
+            System.out.println("Usuário não está logado!");
+        }
+    }
+
+    @Test
+    public void buscaMusica() {
+        buscaMusicaRequest(nomePlayList);
+        if (response.statusCode() != 401) {
+           // list = response.path("items.track.name");
+            Assert.assertTrue(verificaPorNome(nomeMusica), "Música não encontrada.");
+            Assert.assertEquals(response.statusCode(), 200);
+        } else {
+            System.out.println("Usuário não está logado!");
+        }
+    }
+
+    @Test
+    public void buscaArtista()
+    {
+        retornaIdMusica(nomePlayList, nomeMusica);
+        response = given()
+                .accept("appication/json")
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(URL + "/tracks/" + idMusica)
+                .then()
+                .extract()
+                .response();
+        if(response.statusCode() != 401) {
+            Assert.assertEquals(response.statusCode(), 200);
+            list = response.path("artists.name");
+            Assert.assertTrue(verificaPorNome(nomeArtista));
+        }
+        else {
+            System.out.println("Usuário não está logado");
+        }
+    }
+
+    public Boolean verificaPorNome(String nome) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(nome)) {
+                index = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void busPlayListRequest() {
         response = given()
                 .accept("application/json")
                 .contentType("application/json")
@@ -21,6 +86,45 @@ public class Treinamento_Spotify {
                 .then()
                 .extract()
                 .response();
-        Assert.assertEquals(response.statusCode(), 200);
+        list = response.path("items.name");
+    }
+
+    public void buscaMusicaRequest(String nomePlayList)
+    {
+        retornaIdPlayList(nomePlayList);
+        response =
+                given()
+                        .accept("application/json")
+                        .contentType("application/json")
+                        .header("Authorization", "Bearer " + token)
+                        .when()
+                        .get(URL + "/playlists/" + idPlayList + "/tracks")
+                        .then()
+                        .extract()
+                        .response();
+        if (response.statusCode() == 200) {
+            list = response.path("items.track.name");
+        }
+    }
+
+    public void retornaIdPlayList(String nomePlayList) {
+        busPlayListRequest();
+        if (verificaPorNome(nomePlayList)) {
+
+            idPlayList = response.path("items[" + index + "].id");
+        } else {
+            System.out.println("PlayList não encontrada");
+        }
+    }
+
+    public void  retornaIdMusica(String nomePlayList, String nomeMusica)
+    {
+        buscaMusicaRequest(nomePlayList);
+        if(verificaPorNome(nomeMusica)) {
+            idMusica = response.path("items[" + index + "].track.id");
+        }
+        else {
+            System.out.println("Música não encontrada!");
+        }
     }
 }
